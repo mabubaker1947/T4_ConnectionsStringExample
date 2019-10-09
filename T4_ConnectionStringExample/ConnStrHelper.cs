e2a11a1026d2798ca8e01eb1d77ef2c68401004b
Muhammad Abubaker <7279385+mabubaker1947@users.noreply.github.com>
2019-10-09 17:11:11 +0100

    initial commit
    This examples is a dempnstration of how the configuration files can be read through the dynamically generated code via T4 templates.

---
+++ T4_ConnectionStringExample/ConnStrHelper.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+
+using System.Configuration;
+
+
+/*
+This is an auto generated class
+*/
+public static class ConnectionStrings{
+
+ public static string local{
+        get { return ConfigurationManager.ConnectionStrings["local"].ConnectionString;}
+}
+ public static string development{
+        get { return ConfigurationManager.ConnectionStrings["development"].ConnectionString;}
+}
+ public static string QA{
+        get { return ConfigurationManager.ConnectionStrings["QA"].ConnectionString;}
+}
+}
+
+
+
---
+++ T4_ConnectionStringExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace T4_ConnectionStringExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(ConnectionStrings.local);
+            Console.WriteLine(ConnectionStrings.development);
+            Console.WriteLine(ConnectionStrings.QA);
+
+            Console.ReadKey();
+        }
+    }
+}
